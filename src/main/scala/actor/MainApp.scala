bde87501bfef31918075d794663c601c74cb0aab
Basil Mason <basilmason@hotmail.com>
2016-08-19 11:21:13 +0100

    Automaton working with AKKA.

--- src/main/scala/actor/MainApp.scala
+++ src/main/scala/actor/MainApp.scala
@@ -15,63 +15,63 @@ import scala.concurrent.duration.Duration
   */
 class MainApp extends App {
 
-  sealed trait AutomatonMessage
-  case object Calculate extends AutomatonMessage
-  case class Work(rank: Int, file: Int) extends AutomatonMessage
-  case class Result(states: List[Int]) extends AutomatonMessage
-  case class Final(rows: mutable.Map[Int, List[Int]], time: Duration) extends AutomatonMessage
+//  sealed trait AutomatonMessage
+//  case object Calculate extends AutomatonMessage
+//  case class Work(rank: Int, file: Int) extends AutomatonMessage
+//  case class Result(states: List[Int]) extends AutomatonMessage
+//  case class Final(rows: mutable.Map[Int, List[Int]], time: Duration) extends AutomatonMessage
+//
+//
+//  val system = ActorSystem("AutomatonSystem")
+//  val resultHandler = system.actorOf(Props[Handler], name = "resultHandler")
+//
+//
+//  class Coordinator(numWorkers: Int) extends Actor {
+//
+//    var rows: mutable.Map[Int, List[Int]] = mutable.Map()
+//    var results = 0
+//    val start: Long = System.currentTimeMillis()
+//    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(numWorkers)), name = "workerRouter")
+//
+//    override def receive: Receive = {
+//      case Calculate => {
+//        /// divide cube into rows
+//        for {
+//          x <- (1 to 2)
+//          y <- (1 to 2)
+//        }
+//        workerRouter ! Work(x,y)
+//      }
+//      case Result(ss) => {
+//        rows ++=
+//        results += 1
+//
+//        // complete
+//        if (results == numRows) {
+//          val t = (System.currentTimeMillis() - start).millis
+//          resultHandler ! Final(rows, t)
+//          context.stop(self)
+//        }
+//      }
+//    }
+//  }
 
-
-  val system = ActorSystem("AutomatonSystem")
-  val resultHandler = system.actorOf(Props[Handler], name = "resultHandler")
-
-
-  class Coordinator(numWorkers: Int) extends Actor {
-
-    var rows: mutable.Map[Int, List[Int]] = mutable.Map()
-    var results = 0
-    val start: Long = System.currentTimeMillis()
-    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(numWorkers)), name = "workerRouter")
-
-    override def receive: Receive = {
-      case Calculate => {
-        /// divide cube into rows
-        for {
-          x <- (1 to 2)
-          y <- (1 to 2)
-        }
-        workerRouter ! Work(x,y)
-      }
-      case Result(ss) => {
-        rows ++=
-        results += 1
-
-        // complete
-        if (results == numRows) {
-          val t = (System.currentTimeMillis() - start).millis
-          resultHandler ! Final(rows, t)
-          context.stop(self)
-        }
-      }
-    }
-  }
-
-  class Worker extends Actor {
-    override def receive: Receive = {
-      case Work(r, f) => sender ! Results
-    }
-  }
-
-  class Handler extends Actor {
-    override def receive: Receive = {
-      case Final(rs, t) => {
-
-
-        context.system.terminate()
-
-      }
-    }
-  }
-
+  //
+  //  class Worker extends Actor {
+  //    override def receive: Receive = {
+  //      case Work(r, f) => sender ! Results
+  //    }
+  //  }
+  //
+  //  class Handler extends Actor {
+  //    override def receive: Receive = {
+  //      case Final(rs, t) => {
+  //
+  //
+  //        context.system.terminate()
+  //
+  //      }
+  //    }
+  //  }
 
 }
